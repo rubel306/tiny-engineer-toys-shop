3a2bdb1e8a42d0913251086463f0fafe9691f8d7
RubelHasan <rubelhasan306@gmail.com>
2023-10-26 23:23:08 +0100

    Basic routing done

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h2 className="text-center text-5xl my-40 text-red-500">
+        404 ! Ops !! page not found !!{" "}
+      </h2>
+    </div>
+  );
+};
+
+export default ErrorPage;

080ee04d45d2c79a211ad04c87dbb9048d28fe8e
RubelHasan <rubelhasan306@gmail.com>
2023-10-27 03:22:21 +0100

    added home banner section

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import banner from "../../../assets/home-banner.jpg";
+const Banner = () => {
+  return (
+    <div>
+      <img src={banner} className="w-full h-screen" alt="" />
+    </div>
+  );
+};
+
+export default Banner;

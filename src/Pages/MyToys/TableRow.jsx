a6b941b423f17bbdb77b6719ce5ca9a1f84bfb14
RubelHasan <rubelhasan306@gmail.com>
2023-10-30 14:14:36 +0000

    deleted one toys from my-toys

--- src/Pages/MyToys/TableRow.jsx
+++ src/Pages/MyToys/TableRow.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const TableRow = ({ product }) => {
   const {
@@ -13,6 +14,31 @@ const TableRow = ({ product }) => {
     subCategory,
     description,
   } = product;
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/products/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.acknowledged === true) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+            console.log(data);
+          });
+      }
+    });
+  };
   console.log("table row", product);
   return (
     <tr>
@@ -35,7 +61,10 @@ const TableRow = ({ product }) => {
         <button className="rounded-lg text-white px-8 py-3 font-semibold bg-[#7ab317] hover:bg-[#000] mb-4">
           Edit
         </button>
-        <button className="rounded-lg text-white px-8 py-3 font-semibold bg-[#d31900] hover:bg-[#000]">
+        <button
+          onClick={() => handleDelete(_id)}
+          className="rounded-lg text-white px-8 py-3 font-semibold bg-[#d31900] hover:bg-[#000]"
+        >
           Delete
         </button>
       </td>
